b788753b5d5bd227c54a47ada0ea4f4267711deb
6460kab0ss <psubrecary@student.hau.edu.ph>
2025-02-17 11:28:38 +0800

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'template-driven';
+  anytext: string= "";
+submitted: boolean = false;
+
+onSubmit () {
+this.submitted = true;
+  }
+}
